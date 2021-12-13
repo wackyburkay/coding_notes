91501bd4eb8d5e4c2515b007aa6ce069c24119b2
Burkay Sabırsız <burkaysabirsiz@gmail.com>
2021-12-13 02:03:43 +0100

    Updated basics.py

--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
-#SORTING #LIST #TUPLE
+#SORTING #LIST #TUPLE ----------
 
 #Bir listeyi sortlarken, listenin elemanları tuple veya daha uzun şeyler ise, listeyi
 #hangi tuple elemanına göre sortlayacağımızı "tup" argümanı belirler. Örn:
@@ -20,7 +20,7 @@ records.sort(key=lambda x:(x[1], x[0]))
 #Burada diyoruz ki önce skora bak, onlar aynıysa isme bak. Stringleri de alfabetik olarak
 #sıralıyor default halinde. Genellikle istenen de odur.
 
-#FLOATING POINT #STRING FORMATTING #INPUT #STAR OPERATOR #DICTIONARY
+#FLOATING POINT #STRING FORMATTING #INPUT #STAR OPERATOR #DICTIONARY ----------
 
 #Öncelikle, star operator pointer ile alakalı değildir, ama aynı zamanda alakalıdır. Arkasındaki
 #düşünce pointerdan gelmektedir. Ne halta yarar, örnekle gösterelim:
@@ -56,6 +56,41 @@ print(format(average, '.2f'))
 #Buradaki format fonksiyonu, verilen stringi formatlamak için kullanılır. Burada floating point
 #significant digit sayısını belirlemede kullanılmış. Daha enteresan kullanımlar için:
 #https://www.w3schools.com/python/ref_string_format.asp
+
+#DICTIONARY #PARSING #I/O ----------
+
+#Gelen inputları kelimelere bölmek için input().split() kullanılır. Input string sonuçta.
+
+N = int(input())
+commands_arr = []
+for i in range(N):
+  command, *operands = input().split()
+  commands_arr.append([command, operands])
+  
+#Burada kod N tane gelen "komut" ları, komut ve argümanları olacak şekilde bir arraye atar. Ama
+#neden array de dict değil? Dict olsaydı, aynı komuttan 2 tane (farklı değerlerle de olsa) tutamazdık.
+#İlk başta o salak hatayı yaptım, sonra düzelttim.
+
+#TUPLE #LIST #HASH #STRING #UPPERCASE #LOWERCASE
+
+myTuple = tuple(some_list)
+
+#Şeklindeki kod bir list'i tuple'a çevirecektir.
+
+hash(myTuple)
+
+#Şeklindeki kod verilen objenin hash değerini döner. List verirsen listedeki her şeyin tek tek hash
+#değerini dönecektir. Hash, Python'un internal kısımlarında hızlı lookup için kullanılır. Daha ayrıntılı
+#bilgi için buraya bak:
+
+#https://stackoverflow.com/questions/17585730/what-does-hash-do-in-python
+
+if someString.isupper():
+  #do something
+string.lower()
+
+#isupper, islower: stringing bütün elemanları upper ya da lowercase mi diye bakar. Aynı şekilde upper ve
+#lower fonksiyonları da stringin tüm elemanlarını upper ya da lower yaparlar.
 
 
 
