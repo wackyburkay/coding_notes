a22750cab9f65dcae74aa19200b4770016fb7069
Burkay Sabırsız <burkaysabirsiz@gmail.com>
2021-12-13 01:04:29 +0100

    Updated basics.py

--- basics.py
+++ basics.py
@@ -1,3 +1,5 @@
+#SORTING #LIST #TUPLE
+
 #Bir listeyi sortlarken, listenin elemanları tuple veya daha uzun şeyler ise, listeyi
 #hangi tuple elemanına göre sortlayacağımızı "tup" argümanı belirler. Örn:
 
@@ -17,4 +19,43 @@ records.sort(key=lambda x:(x[1], x[0]))
 
 #Burada diyoruz ki önce skora bak, onlar aynıysa isme bak. Stringleri de alfabetik olarak
 #sıralıyor default halinde. Genellikle istenen de odur.
+
+#FLOATING POINT #STRING FORMATTING #INPUT #STAR OPERATOR #DICTIONARY
+
+#Öncelikle, star operator pointer ile alakalı değildir, ama aynı zamanda alakalıdır. Arkasındaki
+#düşünce pointerdan gelmektedir. Ne halta yarar, örnekle gösterelim:
+
+n = int(input())
+  student_marks = {}
+  for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+  query_name = input()
+  
+#HackeRank örneğinin baş kısmı bu. Burada anlamı, "kullanıcı bir line girecek. line içerisindeki
+#ilk kelime name, sonrakileri de bir array halinde al" şeklindedir. Hani arrayler C++'ta ilk
+#elemana işaret eden birer pointer ya, *args ile aynı mantık aslında. Bu operator bazen farklı
+#anlamlarda da kullanılır. Bu linke bak görmek için:
+
+#https://www.tutorialspoint.com/What-does-the-Star-operator-mean-in-Python
+
+#Dict içindeki keye karşılı kgelen değere (ki burada o değer bir tuple/array) şöyle ulaşılır:
+
+grades = student_marks[query_name]
+
+#Kullanıcı verdiği öğrenci listesinden bir isim girince, o isme ait (keye ait) not listesini (value)
+#çekmiş olduk.
+
+average = 0
+for grade in grades:
+  average = average + grade
+average = average / len(grades)
+print(format(average, '.2f'))
+
+#Buradaki format fonksiyonu, verilen stringi formatlamak için kullanılır. Burada floating point
+#significant digit sayısını belirlemede kullanılmış. Daha enteresan kullanımlar için:
+#https://www.w3schools.com/python/ref_string_format.asp
+
+
 
